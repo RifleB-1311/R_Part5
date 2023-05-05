f1205c89a887e8d0a60e668a168c763ef48bf15d
RifleB-1311 <90120265+RifleB-1311@users.noreply.github.com>
2023-05-05 09:23:03 +0700

    Ex 5.5

---
+++ 5.5.R
@@ -0,0 +1,15 @@
+#Cau a
+cach_1 <- factorial(32) / factorial(32-3)
+print(cach_1)
+
+a <- 32
+b <- 3
+c <- 3
+cach_2 = choose(a, b) * factorial(c)
+
+print(cach_2)
+
+#Cau b
+d = 32
+e = 3
+choose(d, e)
