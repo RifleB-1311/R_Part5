b387e4bbbee4b00dd76c7c52d8d5ddd0f62bfd89
RifleB-1311 <90120265+RifleB-1311@users.noreply.github.com>
2023-05-05 09:15:55 +0700

    Ex 5.2

---
+++ 5.2.R
@@ -0,0 +1,8 @@
+#cau a 
+n = 25
+k = 5
+factorial(n)/(factorial(k)*factorial(n-k)) # C(n, k) = n! / (k! * (n-k)!)
+#cau b
+n = 29
+k = 5 
+factorial(n)/(factorial(k)*factorial(n-k))# C(n, k) = n! / (k! * (n-k)!)
