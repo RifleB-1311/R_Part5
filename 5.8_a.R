b19c01b291bf7a54159d67578de9b172931f2599
RifleB-1311 <90120265+RifleB-1311@users.noreply.github.com>
2023-05-05 09:27:48 +0700

    Ex 5.8_a

---
+++ 5.8_a.R
@@ -0,0 +1,15 @@
+n = 2
+k = 2
+factorial(n) / (factorial(k)*factorial(n - k))
+
+n = 3
+k = 2
+factorial(n) / (factorial(k)*factorial(n - k))
+
+n = 4
+k = 2
+factorial(n) / (factorial(k)*factorial(n - k))
+
+n = 5
+k = 2
+factorial(n) / (factorial(k)*factorial(n - k))
